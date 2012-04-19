78ce5b603431ca39116807d6157bc515fe65049d
Stefan van den Berg <chtk@xs4all.nl>
2012-04-19 18:08:07 +0200

    [tnftp] OPTWARE_PREFIX fixes
    Signed-off-by: Stefan van den Berg <chtk@xs4all.nl>

--- make/tnftp.mk
+++ make/tnftp.mk
@@ -131,7 +131,7 @@ $(TNFTP_BUILD_DIR)/.configured: $(DL_DIR)/$(TNFTP_SOURCE) $(TNFTP_PATCHES) make/
 		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
-		--prefix=$(OPTWARE_PREFIX)\
+		--prefix=$(OPTWARE_PREFIX) \
 		--disable-nls \
 		--disable-static \
 	)
@@ -198,11 +198,11 @@ $(TNFTP_IPK): $(TNFTP_BUILD_DIR)/.built
 	rm -rf $(TNFTP_IPK_DIR) $(BUILD_DIR)/tnftp_*_$(TARGET_ARCH).ipk
 	$(MAKE) -C $(TNFTP_BUILD_DIR) install \
 		transform='s:^ftp:tnftp:' \
-		prefix=$(TNFTP_IPK_DIR)$(OPTWARE_PREFIX)\
-		mandircat1=$(TNFTP_IPK_DIR)$(OPTWARE_PREFIX)share/man/man1 
-	chmod +w $(TNFTP_IPK_DIR)$(OPTWARE_PREFIX)bin/tnftp && \
-	$(STRIP_COMMAND) $(TNFTP_IPK_DIR)$(OPTWARE_PREFIX)bin/tnftp && \
-	chmod -w $(TNFTP_IPK_DIR)$(OPTWARE_PREFIX)bin/tnftp
+		prefix=$(TNFTP_IPK_DIR)$(OPTWARE_PREFIX) \
+		mandircat1=$(TNFTP_IPK_DIR)$(OPTWARE_PREFIX)/share/man/man1 
+	chmod +w $(TNFTP_IPK_DIR)$(OPTWARE_PREFIX)/bin/tnftp && \
+	$(STRIP_COMMAND) $(TNFTP_IPK_DIR)$(OPTWARE_PREFIX)/bin/tnftp && \
+	chmod -w $(TNFTP_IPK_DIR)$(OPTWARE_PREFIX)/bin/tnftp
 #	install -d $(TNFTP_IPK_DIR)$(OPTWARE_PREFIX)etc/
 #	install -m 644 $(TNFTP_SOURCE_DIR)/tnftp.conf $(TNFTP_IPK_DIR)$(OPTWARE_PREFIX)etc/tnftp.conf
 #	install -d $(TNFTP_IPK_DIR)$(OPTWARE_PREFIX)etc/init.d
