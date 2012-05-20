d7910c4664a83616859aba179937cb2de9ef4be2
Stefan van den Berg <chtk@xs4all.nl>
2012-05-20 21:13:18 +0200

    [crypto++] OPTWARE_PREFIX fixes
    Signed-off-by: Stefan van den Berg <chtk@xs4all.nl>

--- make/crypto++.mk
+++ make/crypto++.mk
@@ -131,7 +131,7 @@ endif
 		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
-		--prefix=$(OPTWARE_PREFIX)\
+		--prefix=$(OPTWARE_PREFIX) \
 		--disable-nls \
 		--disable-static \
 	)
