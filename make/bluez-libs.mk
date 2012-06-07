e3bb252dc935304b1903af3901a3ee479533b22d
Stefan van den Berg <chtk@xs4all.nl>
2012-06-07 21:49:47 +0100

    [bluez-libs] OPTWARE_PREFIX fixes
    Signed-off-by: Stefan van den Berg <chtk@xs4all.nl>

--- make/bluez-libs.mk
+++ make/bluez-libs.mk
@@ -115,7 +115,7 @@ $(BLUEZ-LIBS_BUILD_DIR)/.configured: $(DL_DIR)/$(BLUEZ-LIBS_SOURCE) $(BLUEZ-LIBS
 		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
-		--prefix=$(OPTWARE_PREFIX)\
+		--prefix=$(OPTWARE_PREFIX) \
 		--disable-nls \
 		--disable-static \
 	)
@@ -183,7 +183,7 @@ $(BLUEZ-LIBS_IPK_DIR)/CONTROL/control:
 $(BLUEZ-LIBS_IPK): $(BLUEZ-LIBS_BUILD_DIR)/.built
 	rm -rf $(BLUEZ-LIBS_IPK_DIR) $(BUILD_DIR)/bluez-libs_*_$(TARGET_ARCH).ipk
 	$(MAKE) -C $(BLUEZ-LIBS_BUILD_DIR) DESTDIR=$(BLUEZ-LIBS_IPK_DIR) install-strip
-	rm -f $(BLUEZ-LIBS_IPK_DIR)$(OPTWARE_PREFIX)lib/libbluetooth.la
+	rm -f $(BLUEZ-LIBS_IPK_DIR)$(OPTWARE_PREFIX)/lib/libbluetooth.la
 	$(MAKE) $(BLUEZ-LIBS_IPK_DIR)/CONTROL/control
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(BLUEZ-LIBS_IPK_DIR)
 
