c16f07b33933460b3edfdeee0558a1c412af7e9b
Stefan van den Berg <chtk@xs4all.nl>
2012-06-10 16:09:26 +0100

    [c-ares] OPTWARE_PREFIX fixes
    Signed-off-by: Stefan van den Berg <chtk@xs4all.nl>

--- make/c-ares.mk
+++ make/c-ares.mk
@@ -123,7 +123,7 @@ $(C_ARES_BUILD_DIR)/.configured: $(DL_DIR)/$(C_ARES_SOURCE) $(C_ARES_PATCHES) ma
 		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
-		--prefix=$(OPTWARE_PREFIX)\
+		--prefix=$(OPTWARE_PREFIX) \
 		--disable-nls \
 		--disable-static \
 		--enable-shared \
