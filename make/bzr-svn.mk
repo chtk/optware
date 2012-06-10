3d60cc2085b465b6eba359f6340c31462aa1f4d1
Stefan van den Berg <chtk@xs4all.nl>
2012-06-10 13:45:09 +0100

    [bzr-svn] OPTWARE_PREFIX fixes
    Signed-off-by: Stefan van den Berg <chtk@xs4all.nl>

--- make/bzr-svn.mk
+++ make/bzr-svn.mk
@@ -120,11 +120,11 @@ $(BZR-SVN_BUILD_DIR)/.configured: $(DL_DIR)/$(BZR-SVN_SOURCE) $(BZR-SVN_PATCHES)
 		echo "[build_ext]"; \
 	        echo "include-dirs=$(STAGING_INCLUDE_DIR):$(STAGING_INCLUDE_DIR)/python2.5"; \
 	        echo "library-dirs=$(STAGING_LIB_DIR)"; \
-	        echo "rpath=$(OPTWARE_PREFIX)lib"; \
+	        echo "rpath=$(OPTWARE_PREFIX)/lib"; \
 		echo "[build_scripts]"; \
-		echo "executable=$(OPTWARE_PREFIX)bin/python2.5"; \
+		echo "executable=$(OPTWARE_PREFIX)/bin/python2.5"; \
 		echo "[install]"; \
-		echo "install_scripts=$(OPTWARE_PREFIX)bin"; \
+		echo "install_scripts=$(OPTWARE_PREFIX)/bin"; \
 	    ) >> setup.cfg; \
 	)
 	# 2.6
@@ -137,11 +137,11 @@ $(BZR-SVN_BUILD_DIR)/.configured: $(DL_DIR)/$(BZR-SVN_SOURCE) $(BZR-SVN_PATCHES)
 		echo "[build_ext]"; \
 	        echo "include-dirs=$(STAGING_INCLUDE_DIR):$(STAGING_INCLUDE_DIR)/python2.6"; \
 	        echo "library-dirs=$(STAGING_LIB_DIR)"; \
-	        echo "rpath=$(OPTWARE_PREFIX)lib"; \
+	        echo "rpath=$(OPTWARE_PREFIX)/lib"; \
 		echo "[build_scripts]"; \
-		echo "executable=$(OPTWARE_PREFIX)bin/python2.6"; \
+		echo "executable=$(OPTWARE_PREFIX)/bin/python2.6"; \
 		echo "[install]"; \
-		echo "install_scripts=$(OPTWARE_PREFIX)bin"; \
+		echo "install_scripts=$(OPTWARE_PREFIX)/bin"; \
 	    ) >> setup.cfg; \
 	)
 	touch $@
@@ -225,7 +225,7 @@ $(PY26-BZR-SVN_IPK_DIR)/CONTROL/control:
 $(PY25-BZR-SVN_IPK): $(BZR-SVN_BUILD_DIR)/.built
 	rm -rf $(PY25-BZR-SVN_IPK_DIR) $(BUILD_DIR)/py25-bzr-svn_*_$(TARGET_ARCH).ipk
 	(cd $(BZR-SVN_BUILD_DIR)/2.5; \
-	    $(HOST_STAGING_PREFIX)/bin/python2.5 setup.py install --root=$(PY25-BZR-SVN_IPK_DIR) --prefix=/opt; \
+	    $(HOST_STAGING_PREFIX)/bin/python2.5 setup.py install --root=$(PY25-BZR-SVN_IPK_DIR) --prefix=$(OPTWARE_PREFIX); \
 	)
 #	$(STRIP_COMMAND) $(PY25-BZR-SVN_IPK_DIR)$(OPTWARE_PREFIX)lib/python2.5/site-packages/bzrlib/*.so
 	$(MAKE) $(PY25-BZR-SVN_IPK_DIR)/CONTROL/control
@@ -234,7 +234,7 @@ $(PY25-BZR-SVN_IPK): $(BZR-SVN_BUILD_DIR)/.built
 $(PY26-BZR-SVN_IPK): $(BZR-SVN_BUILD_DIR)/.built
 	rm -rf $(PY26-BZR-SVN_IPK_DIR) $(BUILD_DIR)/py26-bzr-svn_*_$(TARGET_ARCH).ipk
 	(cd $(BZR-SVN_BUILD_DIR)/2.6; \
-	    $(HOST_STAGING_PREFIX)/bin/python2.6 setup.py install --root=$(PY26-BZR-SVN_IPK_DIR) --prefix=/opt; \
+	    $(HOST_STAGING_PREFIX)/bin/python2.6 setup.py install --root=$(PY26-BZR-SVN_IPK_DIR) --prefix=$(OPTWARE_PREFIX); \
 	)
 #	$(STRIP_COMMAND) $(PY26-BZR-SVN_IPK_DIR)$(OPTWARE_PREFIX)lib/python2.6/site-packages/bzrlib/*.so
 	$(MAKE) $(PY26-BZR-SVN_IPK_DIR)/CONTROL/control
