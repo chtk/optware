a7940122fd1fe7dfad288a73f78c9eb7af9ddd5f
Stefan van den Berg <chtk@xs4all.nl>
2012-06-18 13:53:14 +0100

    [cmdftp] OPTWARE_PREFIX fixes
    Signed-off-by: Stefan van den Berg <chtk@xs4all.nl>

--- make/cmdftp.mk
+++ make/cmdftp.mk
@@ -123,7 +123,7 @@ $(CMDFTP_BUILD_DIR)/.configured: $(DL_DIR)/$(CMDFTP_SOURCE) $(CMDFTP_PATCHES) ma
 		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
-		--prefix=$(OPTWARE_PREFIX)\
+		--prefix=$(OPTWARE_PREFIX) \
 		--disable-nls \
 		--disable-static \
 	)
