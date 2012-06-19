bf9c1e00b2d22e67ecfb4b833ab1d795ef610714
Stefan van den Berg <chtk@xs4all.nl>
2012-06-19 19:20:15 +0100

    [cron] OPTWARE_PREFIX fixes
    Signed-off-by: Stefan van den Berg <chtk@xs4all.nl>

--- make/cron.mk
+++ make/cron.mk
@@ -34,7 +34,7 @@ CRON_IPK_VERSION=9
 
 #
 # CRON_CONFFILES should be a list of user-editable files
-CRON_CONFFILES=$(OPTWARE_PREFIX)etc/crontab $(OPTWARE_PREFIX)etc/init.d/S10cron
+CRON_CONFFILES=$(OPTWARE_PREFIX)/etc/crontab $(OPTWARE_PREFIX)/etc/init.d/S10cron
 
 #
 # CRON_PATCHES should list any patches, in the the order in
@@ -160,28 +160,32 @@ $(CRON_IPK_DIR)/CONTROL/control:
 $(CRON_IPK): $(CRON_BUILD_DIR)/.built
 	rm -rf $(CRON_IPK_DIR) $(BUILD_DIR)/cron_*_$(TARGET_ARCH).ipk
 # 	Install and strip the two executables
-	install -d $(CRON_IPK_DIR)$(OPTWARE_PREFIX)bin
-	install -m  755 $(CRON_BUILD_DIR)/crontab $(CRON_IPK_DIR)$(OPTWARE_PREFIX)bin
-	$(STRIP_COMMAND) $(CRON_IPK_DIR)$(OPTWARE_PREFIX)bin/crontab
-	install -d $(CRON_IPK_DIR)$(OPTWARE_PREFIX)sbin
-	install -m  755 $(CRON_BUILD_DIR)/cron $(CRON_IPK_DIR)$(OPTWARE_PREFIX)sbin
-	$(STRIP_COMMAND) $(CRON_IPK_DIR)$(OPTWARE_PREFIX)sbin/cron
+	install -d $(CRON_IPK_DIR)$(OPTWARE_PREFIX)/bin
+	install -m  755 $(CRON_BUILD_DIR)/crontab $(CRON_IPK_DIR)$(OPTWARE_PREFIX)/bin
+	$(STRIP_COMMAND) $(CRON_IPK_DIR)$(OPTWARE_PREFIX)/bin/crontab
+	install -d $(CRON_IPK_DIR)$(OPTWARE_PREFIX)/sbin
+	install -m  755 $(CRON_BUILD_DIR)/cron $(CRON_IPK_DIR)$(OPTWARE_PREFIX)/sbin
+	$(STRIP_COMMAND) $(CRON_IPK_DIR)$(OPTWARE_PREFIX)/sbin/cron
 # 	Install manuals
-	install -d $(CRON_IPK_DIR)$(OPTWARE_PREFIX)man/man1
-	install -d $(CRON_IPK_DIR)$(OPTWARE_PREFIX)man/man5
-	install -d $(CRON_IPK_DIR)$(OPTWARE_PREFIX)man/man8
-	install -m 644 $(CRON_BUILD_DIR)/crontab.1 $(CRON_IPK_DIR)$(OPTWARE_PREFIX)man/man1
-	install -m 644 $(CRON_BUILD_DIR)/crontab.5 $(CRON_IPK_DIR)$(OPTWARE_PREFIX)man/man5
-	install -m 644 $(CRON_BUILD_DIR)/cron.8    $(CRON_IPK_DIR)$(OPTWARE_PREFIX)man/man8
+	install -d $(CRON_IPK_DIR)$(OPTWARE_PREFIX)/man/man1
+	install -d $(CRON_IPK_DIR)$(OPTWARE_PREFIX)/man/man5
+	install -d $(CRON_IPK_DIR)$(OPTWARE_PREFIX)/man/man8
+	install -m 644 $(CRON_BUILD_DIR)/crontab.1 $(CRON_IPK_DIR)$(OPTWARE_PREFIX)/man/man1
+	install -m 644 $(CRON_BUILD_DIR)/crontab.5 $(CRON_IPK_DIR)$(OPTWARE_PREFIX)/man/man5
+	install -m 644 $(CRON_BUILD_DIR)/cron.8    $(CRON_IPK_DIR)$(OPTWARE_PREFIX)/man/man8
 #	Install default crontab
-	install -d $(CRON_IPK_DIR)$(OPTWARE_PREFIX)etc
-	install -m 600 $(CRON_SOURCE_DIR)/crontab $(CRON_IPK_DIR)$(OPTWARE_PREFIX)etc/crontab
+	install -d $(CRON_IPK_DIR)$(OPTWARE_PREFIX)/etc
+	install -m 600 $(CRON_SOURCE_DIR)/crontab $(CRON_IPK_DIR)$(OPTWARE_PREFIX)/etc/crontab
 #	Install daemon startup file
-	install -d $(CRON_IPK_DIR)$(OPTWARE_PREFIX)etc/init.d
-	install -m 755 $(CRON_SOURCE_DIR)/rc.cron $(CRON_IPK_DIR)$(OPTWARE_PREFIX)etc/init.d/S10cron
+	install -d $(CRON_IPK_DIR)$(OPTWARE_PREFIX)/etc/init.d
+	install -m 755 $(CRON_SOURCE_DIR)/rc.cron $(CRON_IPK_DIR)$(OPTWARE_PREFIX)/etc/init.d/S10cron
 	$(MAKE) $(CRON_IPK_DIR)/CONTROL/control
 	install -m 755 $(CRON_SOURCE_DIR)/postinst $(CRON_IPK_DIR)/CONTROL/postinst
 	install -m 644 $(CRON_SOURCE_DIR)/prerm $(CRON_IPK_DIR)/CONTROL/
+	sed -i -e "s,/opt/,$(OPTWARE_PREFIX)/,g" \
+		$(subst $(OPTWARE_PREFIX),$(CRON_IPK_DIR)$(OPTWARE_PREFIX),$(CRON_CONFFILES)) \
+		$(CRON_IPK_DIR)/CONTROL/postinst \
+		$(CRON_IPK_DIR)/CONTROL/prerm
 	echo $(CRON_CONFFILES) | sed -e 's/ /\n/g' > $(CRON_IPK_DIR)/CONTROL/conffiles
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(CRON_IPK_DIR)
 
