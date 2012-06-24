22e21e8666fdcad73c98acdd80bc5aba49561f64
Stefan van den Berg <chtk@xs4all.nl>
2012-06-24 13:00:21 +0100

    [dash] OPTWARE_PREFIX fixes
    Signed-off-by: Stefan van den Berg <chtk@xs4all.nl>

--- make/dash.mk
+++ make/dash.mk
@@ -122,7 +122,7 @@ $(DASH_BUILD_DIR)/.configured: $(DL_DIR)/$(DASH_SOURCE) $(DASH_PATCHES) make/das
 		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
-		--prefix=$(OPTWARE_PREFIX)\
+		--prefix=$(OPTWARE_PREFIX) \
 		--disable-nls \
 		--disable-static \
 	)
