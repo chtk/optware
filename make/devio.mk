36ae30f38f1c15c476aec323966081c871b896d0
Stefan van den Berg <chtk@xs4all.nl>
2012-06-24 22:55:00 +0200

    [devio] OPTWARE_PREFIX fixes
    Signed-off-by: Stefan van den Berg <chtk@xs4all.nl>

--- make/devio.mk
+++ make/devio.mk
@@ -124,7 +124,7 @@ $(DEVIO_BUILD_DIR)/.configured: $(DL_DIR)/$(DEVIO_SOURCE) $(DEVIO_PATCHES) make/
 		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
-		--prefix=$(OPTWARE_PREFIX)\
+		--prefix=$(OPTWARE_PREFIX) \
 		--disable-nls \
 		--disable-static \
 	)
