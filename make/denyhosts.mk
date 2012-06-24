72d099c8ffecdd27232c295b831f5e42371ab0bc
Stefan van den Berg <chtk@xs4all.nl>
2012-06-24 21:28:58 +0100

    [denyhosts] OPTWARE_PREFIX fixes
    Signed-off-by: Stefan van den Berg <chtk@xs4all.nl>

--- make/denyhosts.mk
+++ make/denyhosts.mk
@@ -24,7 +24,7 @@ DENYHOSTS_IPK_VERSION=5
 
 #
 # DENYHOSTS_CONFFILES should be a list of user-editable files
-DENYHOSTS_CONFFILES=$(OPTWARE_PREFIX)etc/denyhosts.cfg $(OPTWARE_PREFIX)etc/init.d/S01denyhosts
+DENYHOSTS_CONFFILES=$(OPTWARE_PREFIX)/etc/denyhosts.cfg $(OPTWARE_PREFIX)/etc/init.d/S01denyhosts
 
 
 #
@@ -125,15 +125,18 @@ $(DENYHOSTS_IPK_DIR)/CONTROL/control:
 $(DENYHOSTS_IPK): $(DENYHOSTS_BUILD_DIR)/.built
 	rm -rf $(DENYHOSTS_IPK_DIR) $(BUILD_DIR)/denyhosts_*_$(TARGET_ARCH).ipk
 	install -d $(DENYHOSTS_IPK_DIR)/var/run/
-	install -d $(DENYHOSTS_IPK_DIR)$(OPTWARE_PREFIX)etc/
-	install -m 644 $(DENYHOSTS_SOURCE_DIR)/denyhosts.cfg $(DENYHOSTS_IPK_DIR)$(OPTWARE_PREFIX)etc/denyhosts.cfg
-	install -d $(DENYHOSTS_IPK_DIR)$(OPTWARE_PREFIX)etc/init.d
-	install -m 755 $(DENYHOSTS_SOURCE_DIR)/rc.denyhosts $(DENYHOSTS_IPK_DIR)$(OPTWARE_PREFIX)etc/init.d/S01denyhosts
-	install -dv $(DENYHOSTS_IPK_DIR)$(OPTWARE_PREFIX)share/denyhosts
-	cp -r $(DENYHOSTS_BUILD_DIR)/* $(DENYHOSTS_IPK_DIR)$(OPTWARE_PREFIX)share/denyhosts
+	install -d $(DENYHOSTS_IPK_DIR)$(OPTWARE_PREFIX)/etc/
+	install -m 644 $(DENYHOSTS_SOURCE_DIR)/denyhosts.cfg $(DENYHOSTS_IPK_DIR)$(OPTWARE_PREFIX)/etc/denyhosts.cfg
+	install -d $(DENYHOSTS_IPK_DIR)$(OPTWARE_PREFIX)/etc/init.d
+	install -m 755 $(DENYHOSTS_SOURCE_DIR)/rc.denyhosts $(DENYHOSTS_IPK_DIR)$(OPTWARE_PREFIX)/etc/init.d/S01denyhosts
+	install -dv $(DENYHOSTS_IPK_DIR)$(OPTWARE_PREFIX)/share/denyhosts
+	cp -r $(DENYHOSTS_BUILD_DIR)/* $(DENYHOSTS_IPK_DIR)$(OPTWARE_PREFIX)/share/denyhosts
 	$(MAKE) $(DENYHOSTS_IPK_DIR)/CONTROL/control
 	echo $(DENYHOSTS_CONFFILES) | sed -e 's/ /\n/g' > $(DENYHOSTS_IPK_DIR)/CONTROL/conffiles
 	install -m 755 $(DENYHOSTS_SOURCE_DIR)/postinst $(DENYHOSTS_IPK_DIR)/CONTROL/postinst
+	sed -i -e "s,/opt/,$(OPTWARE_PREFIX)/,g" \
+		$(subst $(OPTWARE_PREFIX),$(DENYHOSTS_IPK_DIR)$(OPTWARE_PREFIX), $(DENYHOSTS_CONFFILES)) \
+		$(DENYHOSTS_IPK_DIR)/CONTROL/postinst
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(DENYHOSTS_IPK_DIR)
 
 #
