697a7e07ba73dc55f3425fec9d0e1d39ebdc386c
Stefan van den Berg <chtk@xs4all.nl>
2012-06-27 20:04:38 +0100

    [e2tools] OPTWARE_PREFIX fixes
    Signed-off-by: Stefan van den Berg <chtk@xs4all.nl>

--- make/e2tools.mk
+++ make/e2tools.mk
@@ -121,7 +121,7 @@ $(E2TOOLS_BUILD_DIR)/.configured: $(DL_DIR)/$(E2TOOLS_SOURCE) $(E2TOOLS_PATCHES)
 		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
-		--prefix=$(OPTWARE_PREFIX)\
+		--prefix=$(OPTWARE_PREFIX) \
 		--disable-nls \
 		--disable-static \
 	)
