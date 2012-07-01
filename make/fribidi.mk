01faa26086f9e550435d0680e40288bb8aa304c7
Stefan van den Berg <chtk@xs4all.nl>
2012-07-01 10:15:41 +0100

    [fribidi] OPTWARE_PREFIX fixes
    Signed-off-by: Stefan van den Berg <chtk@xs4all.nl>

--- make/fribidi.mk
+++ make/fribidi.mk
@@ -123,7 +123,7 @@ $(FRIBIDI_BUILD_DIR)/.configured: $(DL_DIR)/$(FRIBIDI_SOURCE) $(FRIBIDI_PATCHES)
 		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
-		--prefix=$(OPTWARE_PREFIX)\
+		--prefix=$(OPTWARE_PREFIX) \
 		--disable-nls \
 		--disable-static \
 	)
