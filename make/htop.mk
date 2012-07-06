bc97fa9893d5765409d90c730dcce6121d5757b4
Stefan van den Berg <chtk@xs4all.nl>
2012-07-06 23:19:38 +0100

    [htop] OPTWARE_PREFIX fixes
    Signed-off-by: Stefan van den Berg <chtk@xs4all.nl>

--- make/htop.mk
+++ make/htop.mk
@@ -147,7 +147,7 @@ endif
 		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
-		--prefix=$(OPTWARE_PREFIX)\
+		--prefix=$(OPTWARE_PREFIX) \
 		--disable-nls \
 		--disable-static \
 		$(HTOP_CONFIGURE_ARGS) \
