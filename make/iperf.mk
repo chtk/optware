41301a02bda2a470326f061500e0ea61d6c197d4
Stefan van den Berg <chtk@xs4all.nl>
2012-07-09 19:37:19 +0100

    [iperf] OPTWARE_PREFIX fixes
    Signed-off-by: Stefan van den Berg <chtk@xs4all.nl>

--- make/iperf.mk
+++ make/iperf.mk
@@ -116,7 +116,7 @@ $(IPERF_BUILD_DIR)/.configured: $(DL_DIR)/$(IPERF_SOURCE) $(IPERF_PATCHES)
 		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
-		--prefix=$(OPTWARE_PREFIX)\
+		--prefix=$(OPTWARE_PREFIX) \
 		--disable-nls \
 	)
 	touch $@
