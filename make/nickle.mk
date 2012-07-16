27299f3bbcb2121185bc13ed169379c213c6e6be
Stefan van den Berg <chtk@xs4all.nl>
2012-07-16 22:01:39 +0100

    [nickle] OPTWARE_PREFIX fixes
    Signed-off-by: Stefan van den Berg <chtk@xs4all.nl>

--- make/nickle.mk
+++ make/nickle.mk
@@ -126,7 +126,7 @@ $(NICKLE_BUILD_DIR)/.configured: $(DL_DIR)/$(NICKLE_SOURCE) $(NICKLE_PATCHES) ma
 		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
-		--prefix=$(OPTWARE_PREFIX)\
+		--prefix=$(OPTWARE_PREFIX) \
 		--disable-nls \
 		--disable-static \
 	)
